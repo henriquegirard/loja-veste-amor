cd0ee68f66866f6a2885bae57031d66d6af8dd3b
henriquegirard <girard92@gmail.com>
2026-06-12 16:45:50 -0300

    fix: injeta axios globalmente para o dashboard funcionar

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,5 +3,7 @@ import 'bootstrap';
 import '@fortawesome/fontawesome-free/js/all.js';
 
 window.Vue = Vue;
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 // The Vue instance is initialized directly in the blade files as requested by the architecture doc.
